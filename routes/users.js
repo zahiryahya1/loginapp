e93199cd3153ef169a410aea883ef8d4f074ec15
zahir yahya <zahir_yahya@yahoo.com>
2017-11-12 13:14:57 -0800

    init app

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// Register
+router.get('/register', function (req, res) {
+    res.render('register');
+});
+
+
+// login
+router.get('/login', function (req, res) {
+    res.render('login');
+});
+
+module.exports = router;
